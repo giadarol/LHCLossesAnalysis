4a9fd55647afd6105829675f6a59586567de182f
giadarol <giovanni.iadarola@cern.ch>
2019-08-02 16:47:43 +0200

    Saving is on

--- 001b_a_look_in_collision_from_database.py
+++ 001b_a_look_in_collision_from_database.py
@@ -23,65 +23,58 @@ group_definitions = [
     {'color':'darkred', 'slots_within_injection': [153, 154, 155, 156, 157], 'n_bunches_in_injection':144},
     ]
 
+outp_folder = '/eos/user/g/giadarol/temp/20190801_losses_wp2/'
+
 # # MD large telescope
 # filln = 7174
 # T_download_h = 5
 # slotrange = np.array([0, 520]) + 520
 # T_h_range_colorplot = [.2, 5.]
 # beam = 1
-# t_detail_h = .3 # at the beginning of the fill 
+# t_detail_h_list = [.3, 2.] # at the beginning of the fill 
 
-# # Physics fill
-# filln = 7145
-# T_download_h = 5
-# t_detail_h = .8
-# beam = 1
-
-# Physics filli (2017)
-filln = 6060
-T_download_h = 15
-slotrange = np.array([0, 520])+1270
-T_h_range_colorplot = [.3, 14.1]
-t_detail_h = 7
-beam = 1 
+# # Physics filli (2017)
+# filln = 6060
+# T_download_h = 15
+# slotrange = np.array([0, 520])+1270
+# T_h_range_colorplot = [.3, 14.1]
+# t_detail_h_list = [0.83, 1.0, 2., 3.5, 3.58, 5.33, 7.17, 8.33, 10, 13]
+# beam = 1 
 
 # # Physics fill
 # filln = 7236
 # T_download_h = 10
 # slotrange = np.array([0, 520])+1270
-# T_h_range_colorplot = [.3, 26.5]
-# t_detail_h = 0.6 # During initial losses
-# t_detail_h = 3
-# t_detail_h = 7
+# T_h_range_colorplot = [.3, 9.5]
+# t_detail_h_list = [0.58, 3, 4, 5, 6, 7]
 # beam = 1 
-# 
+
 # # Physics fill (constant angle)
 # filln = 7266
 # T_download_h = 10
 # slotrange = np.array([0, 520])+1270
 # T_h_range_colorplot = [.3, 9.2]
 # beam = 1
-# t_detail_h = .47
+# t_detail_h_list = [0.5, 2, 3, 4, 5, 6, 7]
 
 # # Physics fill (very long)
 # filln = 7056
 # T_download_h = 30
 # slotrange = np.array([0, 520])+1270
 # T_h_range_colorplot = [.3, 25.9]
-# t_detail_h = 0.6 # During initial losses
-# t_detail_h = 3
-# t_detail_h = 7
+# t_detail_h_list = [0.33, 1, 2,3,4,5,6,7,7.5,8,9,9.5,10,11,12,12.5,13,14,15,16,17,18,19,20,21,22,23,24,25]
 # beam = 1 
 
-# # Only beam 1
-# filln = 6966
-# T_download_h = 5
-# slotrange = np.array([0, 520])+1270
-# T_h_range_colorplot = [.3, 2.77]
+# Only beam 1
+filln = 6966
+T_download_h = 5
+slotrange = np.array([0, 520])+1270
+T_h_range_colorplot = [.3, 2.77]
+t_detail_h_list = [1.7, 2.55, 2.7]
 # t_detail_h = 1.7 # 30 cm, 130 urad, nominal tunes
 # t_detail_h = 2.55 # 25 cm, 130 urad, nominal tunes
 # t_detail_h = 2.7 # 25 cm, 130 urad, modified tunes
-# beam = 1
+beam = 1
   
 # # Only beam 2 
 # filln = 6967
@@ -273,65 +266,71 @@ fig.subplots_adjust(right=.95, left=.05, bottom=.12, top=.81, hspace = 1)
 
 fig.suptitle('Fill %d SB BurnOff corrected lifetime B%d\nFT started on %s\n'%(filln, beam, tref_string))
 
-i_detail = np.argmin(np.abs(t_stamps - (t_start+t_detail_h*3600)))
-t_det_h_found = tc(t_stamps)[i_detail]
-axd = None
-axd2 = None
-if t_det_h_found<tc(t_stamps)[-1]:
-#    figd = plt.figure(100+beam, figsize=(8*1.4,6))
-#    figd.set_facecolor('w')
-#    axd = figd.add_subplot(111, sharex=axslot, sharey=axd)
-
-    i_plot = np.argmin(np.abs(t_det_h_found-tc(t_stamps)))
-    BO_lr_det = BO_loss_rate[i_plot, :]
-    lr_det = loss_rate[i_plot, :]
-    bint_det = bint[i_plot, :]
-
-    mask_filled_det = bint_det>2e10
-    BO_lr_det[~mask_filled_det] = 0.
-    lr_det[~mask_filled_det] = 0.
-
-    lt_BO_avg = 1./(np.sum(BO_lr_det)/np.sum(bint[i_plot]))/3600.
-    lt_other = 1./(np.sum(lr_det-BO_lr_det)/np.sum(bint[i_plot]))/3600.
-
-    # plt.fill_between(np.arange(3564),BO_lr_det, color='green', alpha=0.6, label='Burn off')
-    # plt.fill_between(np.arange(3564), BO_lr_det, lr_det, color='red', alpha=.6, label='Additional losses')
-    # ms.sciy()
-    # axd.set_ylabel('Loss rate [p/s]')
-    # axd.set_xlabel('25 ns slot')
-    # axd.legend(loc='lower right', prop={'size':14})
-    # axd.set_xlim(0,3500)
-    # axd.set_ylim(bottom=0)
-    # axd.grid('on')
-    # figd.subplots_adjust(right=.94, left=.1, bottom=.12, top=.86)
-    # figd.suptitle('Fill %d SB Loss Rate at %.1fh for B%d\nSB started on %s\n'%(filln, t_det_h_found, beam, tref_string))
+for t_detail_h in t_detail_h_list:
+    i_detail = np.argmin(np.abs(t_stamps - (t_start+t_detail_h*3600)))
+    t_det_h_found = tc(t_stamps)[i_detail]
+    axd = None
+    axd2 = None
+    if t_det_h_found<tc(t_stamps)[-1]:
+    #    figd = plt.figure(100+beam, figsize=(8*1.4,6))
+    #    figd.set_facecolor('w')
+    #    axd = figd.add_subplot(111, sharex=axslot, sharey=axd)
     
-
-    figd2 = plt.figure(200+beam, figsize=(8*1.4,6))
-    figd2.clear()
-    figd2.set_facecolor('w')
-    axd2 = figd2.add_subplot(111, sharex=axslot, sharey=axd2)
-
-    axd2.plot(np.arange(3564), BO_lr_det*3600*1e-9, 'b', lw=2, alpha=.4, label='Burn-off')
-    axd2.plot(np.arange(3564), (lr_det-BO_lr_det)*3600*1e-9, 'red', lw=2, label='Other losses')
-    ms.sciy()
-    axd2.set_ylabel(r'Loss rate [10$^9$ p/h]')
-    axd2.set_xlabel('25 ns slot')
-    axd2.legend(loc='upper right', prop={'size':14})
-    axd2.set_xlim(0,3500)
-    axd2.set_ylim(bottom=-.2, top=5.)
-    axd2.grid('on')
-    figd2.subplots_adjust(right=.94, left=.1, bottom=.12, top=.86)
-    figd2.suptitle('Fill %d Loss Rates at %.1fh for B%d\nFT started on %s\n'%(filln, t_det_h_found, beam, tref_string))
-
-if slotrange is not None:
-    axslot.set_xlim(slotrange)
-
-if T_h_range_colorplot is not None:
-    axgr.set_ylim(T_h_range_colorplot)
+        i_plot = np.argmin(np.abs(t_det_h_found-tc(t_stamps)))
+        BO_lr_det = BO_loss_rate[i_plot, :]
+        lr_det = loss_rate[i_plot, :]
+        bint_det = bint[i_plot, :]
+    
+        mask_filled_det = bint_det>2e10
+        BO_lr_det[~mask_filled_det] = 0.
+        lr_det[~mask_filled_det] = 0.
+    
+        lt_BO_avg = 1./(np.sum(BO_lr_det)/np.sum(bint[i_plot]))/3600.
+        lt_other = 1./(np.sum(lr_det-BO_lr_det)/np.sum(bint[i_plot]))/3600.
+    
+        # plt.fill_between(np.arange(3564),BO_lr_det, color='green', alpha=0.6, label='Burn off')
+        # plt.fill_between(np.arange(3564), BO_lr_det, lr_det, color='red', alpha=.6, label='Additional losses')
+        # ms.sciy()
+        # axd.set_ylabel('Loss rate [p/s]')
+        # axd.set_xlabel('25 ns slot')
+        # axd.legend(loc='lower right', prop={'size':14})
+        # axd.set_xlim(0,3500)
+        # axd.set_ylim(bottom=0)
+        # axd.grid('on')
+        # figd.subplots_adjust(right=.94, left=.1, bottom=.12, top=.86)
+        # figd.suptitle('Fill %d SB Loss Rate at %.1fh for B%d\nSB started on %s\n'%(filln, t_det_h_found, beam, tref_string))
+        
+    
+        figd2 = plt.figure(200+beam, figsize=(8*1.4,6))
+        figd2.clear()
+        figd2.set_facecolor('w')
+        axd2 = figd2.add_subplot(111, sharex=axslot, sharey=axd2)
+    
+        axd2.plot(np.arange(3564), BO_lr_det*3600*1e-9, 'b', lw=2, alpha=.4, label='Burn-off')
+        axd2.plot(np.arange(3564), (lr_det-BO_lr_det)*3600*1e-9, 'red', lw=2, label='Other losses')
+        ms.sciy()
+        axd2.set_ylabel(r'Loss rate [10$^9$ p/h]')
+        axd2.set_xlabel('25 ns slot')
+        axd2.legend(loc='upper right', prop={'size':14})
+        axd2.set_xlim(0,3500)
+        axd2.set_ylim(bottom=-.2, top=5.)
+        axd2.grid('on')
+        figd2.subplots_adjust(right=.94, left=.1, bottom=.12, top=.86)
+        figd2.suptitle('Fill %d Loss Rates at %.1fh for B%d\nFT started on %s\n'%(filln, t_det_h_found, beam, tref_string))
+    
+    if slotrange is not None:
+        axslot.set_xlim(slotrange)
+    
+    if T_h_range_colorplot is not None:
+        axgr.set_ylim(T_h_range_colorplot)
+    
+    
+    def savefigures(folder, beam, filln, t_det_h_found, tag=''):
+        fig.savefig(folder+'/fill%d_%s_beam%s_evol.png'%(filln, tag, beam), dpi=200)
+        figd2.savefig(folder+'/fill%d_%s_beam%s_detals_at_%.2fh.png'%(filln, tag, beam, t_det_h_found), dpi=200)
+    
+    
+    if outp_folder is not None:
+        savefigures(folder=outp_folder, beam=beam, filln=filln, t_det_h_found=t_det_h_found)
 
 plt.show()
-
-def savefigures(folder, beam, filln, t_det_h_found, tag=''):
-    fig.savefig(folder+'/fill%d_%s_beam%s_evol.png'%(filln, tag, beam, dpi=200)
-    figd2.savefig(folder+'/fill%d_%s_beam%s_detals_at_%.2fh.png'%(filln, tag, beam, t_det_h_found), dpi=200)
